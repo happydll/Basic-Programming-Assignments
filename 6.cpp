ab55470aaa13996146022703eb048c8de92727fb
happydll <85785507+happydll@users.noreply.github.com>
2021-10-20 00:27:40 +0300

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	int x;
+	x=65;
+	if (x==64 || x!=65 || x==66)
+		{
+		std::cout << "Statement 1" << std::endl;
+		}
+	else 
+		{
+		std::cout << "Statement 2" << std::endl;
+		}
+}	
+
