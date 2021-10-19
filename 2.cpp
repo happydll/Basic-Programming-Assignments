5260d250191b2e19e6be25202d46da990b5fbdbb
happydll <85785507+happydll@users.noreply.github.com>
2021-10-20 00:23:47 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+int main() 
+	{ 
+		int x; 
+		std::cout << "Pick an integer:" << std::endl; 
+		std::cin >> x; 
+			if (x==7) 
+				{ 
+			std::cout << "You win the lottery!" << std::endl; 
+			std::cout << "Thank you for playing the C++ lottery" << std::endl; 
+				}
+			else 
+				{ 
+			std::cout << "Thank you for playing the C++ lottery" << std::endl;	
+				}
+	
+		
+	}
