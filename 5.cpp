e08b48a0586ae543a005648e18cbc705fcdc8c85
happydll <85785507+happydll@users.noreply.github.com>
2021-10-20 00:27:18 +0300

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+	
+	int x;
+	x=7;
+	if (!(x==7))
+		{
+		std::cout << "Statement 1" << std::endl;
+		}
+	else 
+		{
+		std::cout << "Statement 2" << std::endl;
+		}
+
+}
